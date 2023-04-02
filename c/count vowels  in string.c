d7f9e9a353c4ec6b07e758a764546b4639f569be
subedi-aarav <aaravsubedi123@gmail.com>
2023-04-02 21:27:02 +0545

    vowel count added to c

---
+++ c/count vowels  in string.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	int i,j,k,count=0,len;
+	char a[100];
+	char v[11] = "aeiouAEIOU";
+	printf("Enter the string: ");
+	gets(a);
+	len= strlen(a);
+	for(i=0;i<len;i++){
+		for(j=0;j<10;j++){
+			if(a[i]==v[j]){
+				++count;
+			}
+		}
+	}
+	printf("Number of vowels char = %d",count);
+	return 0;
+}
