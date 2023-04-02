91f47880edf9cbde9442657d24d2ab17c0c29e08
subedi-aarav <aaravsubedi123@gmail.com>
2023-04-02 21:29:54 +0545

    reverse string added to c

---
+++ c/Reverse_string.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	int i,j,len;
+	char a[100];
+	char r[100];
+	printf("Enter the sting: ");
+	gets(a);
+	puts(a);
+	len = strlen(a);
+//	printf("%d",len);
+	
+	j=0;
+	i=len-1;
+	
+	while(i>=0){
+		r[j] = a[i];
+		j++;
+		i--;
+	}	
+	puts(r);	
+	return 0;
+}
